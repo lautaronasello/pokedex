d300c9b9e3e801df4305d7c18727f6247468aa7d
Lautaro Nasello <lautaronasello@gmail.com>
2021-05-12 09:22:13 -0300

    12/5

--- src/Pokelist.js
+++ src/Pokelist.js
@@ -2,8 +2,10 @@ import axios from 'axios';
 import React, { useState, useEffect } from 'react';
 import Pagination from './components/Pagination';
 import Pokemon from './components/Pokemon';
+import { useHistory } from 'react-router';
 
 function Pokelist() {
+  const history = useHistory();
   const [pokemon, setPokemon] = useState([]);
   const [currentPage, setCurrentPage] = useState(
     'https://pokeapi.co/api/v2/pokemon'
@@ -11,6 +13,7 @@ function Pokelist() {
   const [nextPage, setNextPage] = useState();
   const [prevPage, setPrevPage] = useState();
   const [loading, setLoading] = useState(true);
+  const [pokemonName, setPokemonName] = useState('');
 
   useEffect(() => {
     let cancel;
@@ -24,6 +27,8 @@ function Pokelist() {
         setNextPage(res.data.next);
         setPrevPage(res.data.previous);
         setPokemon(res.data.results.map((p) => p.name));
+        setPokemonName(res.data.results[0].name);
+        console.log(res.data);
       });
 
     return () => cancel();
@@ -34,8 +39,8 @@ function Pokelist() {
       <div className='container'>
         <div className='row text-center justify-content-center'>
           <div className='col-6'>
-            <div class='spinner-border text-warning mt-5' role='status'>
-              <span class='visually-hidden' />
+            <div className='spinner-border text-warning mt-5' role='status'>
+              <span className='visually-hidden' />
             </div>
           </div>
         </div>
@@ -50,11 +55,18 @@ function Pokelist() {
     setCurrentPage(prevPage);
   }
 
+  function goToPokemon() {
+    history.push({
+      pathname: '/dashboard',
+    });
+    console.log('apretaste gotopokemon ' + pokemonName);
+  }
+
   return (
     <div className='container'>
       <div className='row text-center justify-content-center'>
         <div className='col-6'>
-          <Pokemon pokemon={pokemon} />
+          <Pokemon pokemon={pokemon} goToPokemon={goToPokemon} />
           <Pagination
             goToNextPage={nextPage ? goToNextPage : null}
             goToPrevPage={prevPage ? goToPrevPage : null}
---
+++ src/components/Pokefunction.js
@@ -0,0 +1,6 @@
+import React from 'react';
+
+export default function Pokefunction(pokemonName) {
+  var pokename = pokemonName;
+  return <div>{pokename}</div>;
+}
