f86f5088a606f2f4a556efd70596405ac26186aa
Lautaro Nasello <lautaronasello@gmail.com>
2021-05-12 09:21:57 -0300

    onClick added

--- src/components/Pokemon.js
+++ src/components/Pokemon.js
@@ -1,10 +1,11 @@
 import React from 'react';
-
-export default function Pokemon({ pokemon }) {
+export default function Pokemon({ pokemon, goToPokemon }) {
   return (
     <div>
       {pokemon.map((p) => (
-        <div key={p}>{p} </div>
+        <div key={p}>
+          <button onClick={goToPokemon}>{p}</button>
+        </div>
       ))}
     </div>
   );
