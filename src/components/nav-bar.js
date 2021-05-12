c4bba8dda7bc094602124b1b846173ef94f0b642
Lautaro Nasello <lautaronasello@gmail.com>
2021-05-11 23:51:04 -0300

    simple nav

--- src/components/nav-bar.js
+++ src/components/nav-bar.js
@@ -5,15 +5,15 @@ export default class NavBar extends Component {
   render() {
     return (
       <Nav
-        className='justify-content-center mt-3'
+        className='justify-content-center mt-3 border border-warning'
         variant='pills'
         defaultActiveKey='/home'
       >
         <Nav.Item>
-          <Nav.Link href='/search'>Search Pokémon</Nav.Link>
+          <Nav.Link href='/dashboard'>Search Pokémon</Nav.Link>
         </Nav.Item>
         <Nav.Item>
-          <Nav.Link eventKey='/pokemons'>Pokémons List</Nav.Link>
+          <Nav.Link href='/pokelist'>Pokémons List</Nav.Link>
         </Nav.Item>
       </Nav>
     );
