94439c03c346934026a345b5c3c83d775784207b
Lautaro Nasello <lautaronasello@gmail.com>
2021-05-11 23:50:34 -0300

    add react-router-dom and layout

--- src/App.js
+++ src/App.js
@@ -2,16 +2,20 @@ import React from 'react';
 import Dashboard from './Dashboard.js';
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 import Layout from './components/Layout.js';
+import Landing from './Landing.js';
+import Pokelist from './Pokelist.js';
 
 function App() {
   return (
     <div>
       <Router>
-        <Layout>
-          <Switch>
-            <Route exact path='/' component={Dashboard} />
-          </Switch>
-        </Layout>
+        <Switch>
+          <Route exact path='/' component={Landing} />
+          <Layout>
+            <Route exact path='/dashboard' component={Dashboard} />
+            <Route exact path='/pokelist' component={Pokelist} />
+          </Layout>
+        </Switch>
       </Router>
     </div>
   );
